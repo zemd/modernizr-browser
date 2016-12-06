54859eac0943e5ff2e6b66e5be515067c10987e4
Dmitry Zelenetskiy <dmitry.zelenetskiy@gmail.com>
2016-12-06 22:31:34 +0200

    adding package to npm

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-modernizr-browser
-=================
+# modernizr-browser
+
+> Browser version detection extension for modernizr
+
+## Installation
+
+```bash
+npm install modernizr-browser --save
+```
+
+or 
+
+```bash
+yarn add modernizr-browser
+```
+
+or
+
+```bash
+bower install modernizr-browser
+```
+
+## Usage
+
+```html
+<script src="path/to/modernizr.js"></script>
+<script src="https://unpkg.com/modernizr-browser@1.0.0/modernizr-browser.js"></script>
+```
+
+
+## License
+
+modernizr-browser is released under the Apache-2.0 license.
+
+## Donate
+
+[![](https://img.shields.io/badge/patreon-donate-yellow.svg)](https://www.patreon.com/red_rabbit)
+[![](https://img.shields.io/badge/flattr-donate-yellow.svg)](https://flattr.com/profile/red_rabbit)
+
--- modernizr-browser.js
+++ modernizr-browser.js
@@ -19,101 +19,101 @@
  * (1) version is for Opera because its ua formatted like Opera/9.80
  */
 (function (global) {
-    'use strict';
+  'use strict';
 
-    var Modernizr = global.Modernizr,
-        navigator = global.navigator,
-        userAgent = (global.userAgent || navigator.userAgent).toLowerCase(),
-        browser = {},
-        version = browser.version = (userAgent.match(new RegExp(".*(?:rv|chrome|webkit|version|ie)[\/: ](.+?)([ \\);]|$)")) || [])[1], /* 1 */
-        isMobile = false,
-        platformName,
-        document = global.document,
-        docElement = document.documentElement;
+  var Modernizr = global.Modernizr,
+    navigator = global.navigator,
+    userAgent = (global.userAgent || navigator.userAgent).toLowerCase(),
+    browser = {},
+    version = browser.version = (userAgent.match(new RegExp(".*(?:rv|chrome|webkit|version|ie)[\/: ](.+?)([ \\);]|$)")) || [])[1], /* 1 */
+    isMobile = false,
+    platformName,
+    document = global.document,
+    docElement = document.documentElement;
 
-    Modernizr.addTest('mobile', function () {
-        var isIphone = new RegExp("iphone").test(userAgent),
-            isIpad = new RegExp("ipad").test(userAgent),
-            isIpod = new RegExp("ipod").test(userAgent),
-            isAndroid = new RegExp("android").test(userAgent),
-            isMobileSafari = new RegExp("apple.*mobile.*safari").test(userAgent);
+  Modernizr.addTest('mobile', function () {
+    var isIphone = new RegExp("iphone").test(userAgent),
+      isIpad = new RegExp("ipad").test(userAgent),
+      isIpod = new RegExp("ipod").test(userAgent),
+      isAndroid = new RegExp("android").test(userAgent),
+      isMobileSafari = new RegExp("apple.*mobile.*safari").test(userAgent);
 
-        isMobile = (isMobileSafari && (isIphone || isIpad || isIpod)) || isAndroid;
+    isMobile = (isMobileSafari && (isIphone || isIpad || isIpod)) || isAndroid;
 
-        if (!isMobile) {
-            return false;
-        }
-
-        Modernizr.addTest('iphone', function () {
-            return isIphone;
-        });
-        Modernizr.addTest('ipod', function () {
-            return isIpod;
-        });
-        Modernizr.addTest('ipad', function () {
-            return isIpad;
-        });
-        Modernizr.addTest('android', function () {
-            return isAndroid;
-        });
-
-        return isMobile;
-    });
-
-    Modernizr.addTest('desktop', function () {
-        if (Modernizr.isMobile) {
-            return false;
-        }
-
-        Modernizr.addTest('ie', function () {
-            if (navigator.appName === 'Microsoft Internet Explorer') {
-                var re = new RegExp("(MSIE|msie) ([0-9]{1,}[\\.0-9]{0,})");
-                /*jslint eqeq: true */
-                /*jshint eqnull: true */
-                if (re.exec(userAgent) != null) {
-                    docElement.className += ' ie' + parseFloat(RegExp.$2);
-                }
-                return true;
-            } else if (navigator.userAgent.match(/\s+Trident\/(\d+)\./)) {
-                re = /\s+rv:(\d+\.\d+)/;
-                /*jslint eqeq: true */
-                /*jshint eqnull: true */
-                if (re.exec(userAgent) != null) {
-                    docElement.className += ' ie' + parseFloat(RegExp.$1);
-                }
-                return true;
-            }
-            return false;
-        });
-
-        Modernizr.addTest('firefox', function () {
-            return new RegExp("mozilla").test(userAgent) && !new RegExp("(compatible|webkit|msie)").test(userAgent);
-        });
-
-        Modernizr.addTest('webkit', function () {
-            return new RegExp("webkit").test(userAgent);
-        });
-
-        Modernizr.addTest('chrome', function () {
-            return new RegExp("chrome").test(userAgent);
-        });
-
-        Modernizr.addTest('opera', function () {
-            return new RegExp("opera").test(userAgent);
-        });
-        return true;
-    });
-
-    platformName = new RegExp("windows").test(userAgent) ? 'windows' :
-        new RegExp("macintosh").test(userAgent) || (new RegExp("mac os x").test(userAgent) && !new RegExp("like mac os x").test(userAgent)) ? 'macos' :
-            new RegExp("(linux | x11)").test(userAgent) ? 'linux' :
-                false;
-
-    if (platformName) {
-        Modernizr.addTest(platformName, function () {
-            return true;
-        });
+    if (!isMobile) {
+      return false;
     }
+
+    Modernizr.addTest('iphone', function () {
+      return isIphone;
+    });
+    Modernizr.addTest('ipod', function () {
+      return isIpod;
+    });
+    Modernizr.addTest('ipad', function () {
+      return isIpad;
+    });
+    Modernizr.addTest('android', function () {
+      return isAndroid;
+    });
+
+    return isMobile;
+  });
+
+  Modernizr.addTest('desktop', function () {
+    if (Modernizr.isMobile) {
+      return false;
+    }
+
+    Modernizr.addTest('ie', function () {
+      if (navigator.appName === 'Microsoft Internet Explorer') {
+        var re = new RegExp("(MSIE|msie) ([0-9]{1,}[\\.0-9]{0,})");
+        /*jslint eqeq: true */
+        /*jshint eqnull: true */
+        if (re.exec(userAgent) != null) {
+          docElement.className += ' ie' + parseFloat(RegExp.$2);
+        }
+        return true;
+      } else if (navigator.userAgent.match(/\s+Trident\/(\d+)\./)) {
+        re = /\s+rv:(\d+\.\d+)/;
+        /*jslint eqeq: true */
+        /*jshint eqnull: true */
+        if (re.exec(userAgent) != null) {
+          docElement.className += ' ie' + parseFloat(RegExp.$1);
+        }
+        return true;
+      }
+      return false;
+    });
+
+    Modernizr.addTest('firefox', function () {
+      return new RegExp("mozilla").test(userAgent) && !new RegExp("(compatible|webkit|msie)").test(userAgent);
+    });
+
+    Modernizr.addTest('webkit', function () {
+      return new RegExp("webkit").test(userAgent);
+    });
+
+    Modernizr.addTest('chrome', function () {
+      return new RegExp("chrome").test(userAgent);
+    });
+
+    Modernizr.addTest('opera', function () {
+      return new RegExp("opera").test(userAgent);
+    });
+    return true;
+  });
+
+  platformName = new RegExp("windows").test(userAgent) ? 'windows' :
+    new RegExp("macintosh").test(userAgent) || (new RegExp("mac os x").test(userAgent) && !new RegExp("like mac os x").test(userAgent)) ? 'macos' :
+      new RegExp("(linux | x11)").test(userAgent) ? 'linux' :
+        false;
+
+  if (platformName) {
+    Modernizr.addTest(platformName, function () {
+      return true;
+    });
+  }
 
 }(this));
 
